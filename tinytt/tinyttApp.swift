1e5f5c47c3bfae59996d56feaeebfd5ffe1b8968
arsh <30162549+arshtsingh@users.noreply.github.com>
2021-02-06 20:49:34 -0600

    init

---
+++ tinytt/tinyttApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinyttApp.swift
+//  tinytt
+//
+//  Created by limitless on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct tinyttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
